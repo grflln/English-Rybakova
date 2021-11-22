712ed21bf639de0118f43f360b7c568bb86469e2
grflln <grflln.r@gmail.com>
2021-11-22 21:18:18 +0300

    adding popup and end up with desktop

--- images/js/main.js
+++ images/js/main.js
@@ -3,4 +3,15 @@ $('document').ready(() => {
     collapsible: true,
     active: false
     });
+
+    $('.buy-button').click(function (e) { 
+        e.preventDefault();
+        $('.modal-container').css('display', 'flex');     
+    });
+
+    $('#modal-container, #close-mark').click(function (e) { 
+        if (e.target.id === 'modal-container' || e.target.id === 'close-mark') {
+        $('.modal-container').hide();     
+        }
+    });
 });
